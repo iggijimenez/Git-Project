964c70bc4eb4be5b91e73c1541b89d61bd07414c
Iggi Jimenez <89427749+iggijimenez@users.noreply.github.com>
2024-02-18 21:05:02 -0800

    changed button to purple

--- Git Project/ContentView.swift
+++ Git Project/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
         NavigationView {
             NavigationLink(destination: ModelVIew(), label: {
                 Text("Press Next")
-            }).foregroundColor(.red)
+            }).foregroundColor(.purple)
             .navigationTitle("Xcode and Git")
         }
     }
